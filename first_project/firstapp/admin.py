b6ad6858b6195a574ce958a22230767469cd9750
xinzefan <xinzefan813@gmail.com>
2023-11-12 19:44:02 -0800

    firstapp

---
+++ first_project/firstapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from firstapp.models import UserProfileInfo
+# Register your models here.
+# admin.site.register(AccessRecord)
+# admin.site.register(Topic)
+# admin.site.register(Webpage)
+admin.site.register(UserProfileInfo)
